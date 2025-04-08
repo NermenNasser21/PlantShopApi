a3ef0d3d5e52b823dd080fc1027884422ae18828
SaraMohameddSayed <saramohamedsci884@gmail.ccom>
2025-04-09 00:39:24 +0200

    Category-DelivaryDetails

---
+++ Models/DelivaryDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class DelivaryDetails
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+    }
+}
