0c238d2c77728914e6d7b21dd9b7655712aa23c7
Mirna-Alfy <mernaalfy30@gmail.com>
2025-04-09 02:04:12 +0200

    Order

--- Models/Models/Order.cs
+++ Models/Models/Order.cs
@@ -11,7 +11,14 @@ namespace Models
     public class Order
     {
         public int Id { get; set; }
-        public PaymentWays Name { get; set; }
+        public PaymentWays PaymentWay { get; set; }
+        public decimal DeliveryPrice { get; set; }
+        public string Picture { get; set; }
+        public string City { get; set; }
+        public string Note { get; set; }
+        public decimal OrderPrice { get; set; }
+        public string UserId { get; set; }
+
 
     }
 }
