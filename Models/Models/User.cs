7a6718d3064a590445b777a2c9fd47d45f2af444
Mirna-Alfy <mernaalfy30@gmail.com>
2025-04-15 02:22:12 +0200

    Context

--- Models/Models/User.cs
+++ Models/Models/User.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Models
 {
-    public class User 
+    public class User :IdentityUser
     {
         public string Id { get; set; }
         public string? FirstName { get; set; }
