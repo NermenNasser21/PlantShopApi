815d2d55075418a2d741abb1d66d9a90314824ea
NermenNasser21 <nermen.nasser218@gmail.com>
2025-04-22 22:08:52 +0200

    Creating managers for the models

---
+++ Managers/MainManager.cs
@@ -0,0 +1,81 @@
+﻿using InfraStructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Managers
+{
+    public class MainManager <T> where T :class
+    {
+        private readonly PlantContext dbContext;
+        private DbSet<T> dbset;
+
+        public MainManager(PlantContext _dbContext)
+        {
+            dbContext= _dbContext;
+            dbset =dbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll() 
+        {
+            return dbset.AsQueryable();
+        }
+
+        public async Task<T> GetOne(object _Id)
+        {
+           
+            return await dbset.FindAsync(_Id);
+        }
+
+        public async Task<bool> Add(T _Item)
+        {
+            try
+            {
+                await dbset.AddAsync(_Item);
+                await dbContext.SaveChangesAsync();
+                return true;
+
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> Update(T _Item)
+        {
+            try
+            {
+                 dbset.Update(_Item);
+                await dbContext.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+            }
+        }
+
+        public async Task<bool> Delete(T _Item)
+        {
+            try
+            {
+                dbset.Remove(_Item);
+                await dbContext.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
--- PlantShopApi/Program.cs
+++ PlantShopApi/Program.cs
@@ -1,4 +1,5 @@
 using InfraStructure;
+using Managers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Models;
@@ -11,9 +12,28 @@ builder.Services.AddDbContext<PlantContext>(c => { c.UseSqlServer(builder.Config
                        .UseLazyLoadingProxies(); },ServiceLifetime.Scoped);
 
 builder.Services.AddControllers();
+
+//Add Your Manager 
+builder.Services.AddScoped<CartManager>();
+builder.Services.AddScoped<CartProductManager>();
+builder.Services.AddScoped<CategoryManager>();
+builder.Services.AddScoped<DelivaryDetailsManager>();
+builder.Services.AddScoped<OrderedProductManager>();
+builder.Services.AddScoped<OrderManager>();
+builder.Services.AddScoped<PhoneNumberManager>();
+builder.Services.AddScoped<ProductManager>();
+builder.Services.AddScoped<ReviewManager>();
+builder.Services.AddScoped<TokenManager>();
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); //need configuration to swagger 
+
+
+//jwttttt
+
 
 var app = builder.Build();
 
