c6d5a1e0a445c3f450e0ddf24f5c752ce5ee2cf3
Mirna-Alfy <mernaalfy30@gmail.com>
2025-04-29 00:14:54 +0300

    Tool configuration and relation

--- Models/Models/Tool.cs
+++ Models/Models/Tool.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +17,32 @@ namespace Models.Models
         public int Quantity { get; set; }
         public decimal? Rate { get; set; }
         public string? Image { get; set; }
+        public int ToolUsageId { get; set; }
+        public virtual ToolUsage ToolUsage { get; set; }
     }
+
+    public class ToolConfiguration : IEntityTypeConfiguration<Tool>
+    {
+        public void Configure(EntityTypeBuilder<Tool> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Quantity)
+                .IsRequired();
+            builder.Property(b => b.Description)
+                .IsRequired()
+                .HasMaxLength(200);
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(70);
+            builder.Property(b => b.Price)
+               .IsRequired();
+
+            builder.HasOne(b => b.ToolUsage)
+                .WithMany(b => b.Tools)
+                .HasForeignKey(b => b.ToolUsageId);
+
+
+        }
+    }
+
 }
